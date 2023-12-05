a950dec21060f67d80e03b3ef779739ed4b674da
priya-123-123 <supriyabasireddy@gmail.com>
2023-12-05 14:30:38 +0530

    Changed LoginTest

---
+++ HybridPractice/src/test/java/practice/HybridPractice/tests/DemoNopCommerceLoginTest.java
@@ -0,0 +1,40 @@
+package practice.HybridPractice.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import practice.HybridPractice.pages.BaseClass;
+import practice.HybridPractice.pages.DemoNopCommerceLoginPage;
+import practice.HybridPractice.utility.BrowserFactory;
+
+public class DemoNopCommerceLoginTest extends BaseClass {
+	
+
+	
+	
+	@Test(enabled=true)
+	
+	public void loginApplication() {
+		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		DemoNopCommerceLoginPage lp=PageFactory.initElements(driver,DemoNopCommerceLoginPage.class);
+		lp.loginToApplication("supriya", "pass@123");
+		
+		
+	}
+	
+	@Test(enabled = false)
+	
+	public void brokenlinks() {
+		DemoNopCommerceLoginPage lp=PageFactory.initElements(driver,DemoNopCommerceLoginPage.class);
+		lp.findBrokenLinks();
+	}
+	
+	
+	
+
+}
